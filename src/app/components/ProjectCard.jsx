3061c92c2c1a433b5486a24a9ddb5e46fc8a109e
Faraz Khan <farazkhanaa2555@gmail.com>
2023-10-14 23:57:43 +0500

    Footer Completed

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,15 +1,34 @@
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 import React from "react";
 
-const ProjectCard = ({ imgUrl, title, description }) => {
+const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
       <div
-        className="h-52 md:h-72"
+        className="h-52 md:h-72 rounded-t-xl relative group"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      ></div>
-      <div>
-        <h5>{title}</h5>
-        <p>{description}</p>
+      >
+        <div className="overlay justify-center items-center gap-1 absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
+          <Link
+            href={gitUrl}
+            className="h-14 w-14 border-2 rounded-full border-[#ADB7BE] flex items-center justify-center hover:border-white group/link"
+          >
+            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] cursor-pointer group-hover/link:text-white" />
+          </Link>
+
+          {/* Eye Icon */}
+          <Link
+            href={previewUrl}
+            className="h-14 w-14 border-2 rounded-full border-[#ADB7BE] flex items-center justify-center hover:border-white group/link"
+          >
+            <EyeIcon className="h-10 w-10 text-[#ADB7BE] cursor-pointer group-hover/link:text-white" />
+          </Link>
+        </div>
+      </div>
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-[#ADB7BE]">{description}</p>
       </div>
     </div>
   );
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import projectsData from "../CardData";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
   return (
     <>
-      <h1>Projects Section</h1>
+      <h2 className="text-white text-4xl mt-4 text-center font-semibold">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row items-center justify-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {filteredProjects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            imgUrl={project.image}
+            description={project.description}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />
+        ))}
+      </div>
     </>
   );
 };
